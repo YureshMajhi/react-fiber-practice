c5af7fd1f94a0448ffb5f4b1e872a23d76771e86
Yuresh <yureshmee@gmail.com>
2025-04-29 17:13:17 +0545

    interactions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import { Canvas, useFrame } from "@react-three/fiber";
 
@@ -18,6 +18,32 @@ const Cube = ({ position, size, color }) => {
   );
 };
 
+const Sphere = ({ position, size, color }) => {
+  const ref = useRef();
+
+  const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const speed = isHovered ? 1.5 : 0.4;
+
+  useFrame((state, delta) => {
+    ref.current.rotation.y += delta * speed;
+  });
+  return (
+    <mesh
+      position={position}
+      ref={ref}
+      onPointerEnter={(e) => (e.stopPropagation(), setIsHovered(true))}
+      onPointerLeave={() => setIsHovered(false)}
+      onClick={() => setIsClicked(!isClicked)}
+      scale={isClicked ? 1.5 : 1}
+    >
+      <sphereGeometry args={size} />
+      <meshStandardMaterial color={isHovered ? "yellow" : "red"} wireframe />
+    </mesh>
+  );
+};
+
 function App() {
   return (
     <>
@@ -25,11 +51,14 @@ function App() {
         <directionalLight position={[0, 0, 2]} intensity={0.5} />
         <ambientLight intensity={0.4} />
 
-        <Cube position={[0, 0, 0]} size={[1, 1, 1]} color={"pink"} />
-        <Cube position={[-2, 0, 0]} size={[1, 1, 1]} color={"orange"} />
-        <Cube position={[0, 2, 0]} size={[1, 1, 1]} color={"red"} />
-        <Cube position={[2, 0, 0]} size={[1, 1, 1]} color={"yellow"} />
-        <Cube position={[0, -2, 0]} size={[1, 1, 1]} color={"brown"} />
+        <group position={[3, 0, 0]}>
+          <Cube position={[0, 0, 0]} size={[1, 1, 1]} color={"pink"} />
+          {/* <Cube position={[-2, 0, 0]} size={[1, 1, 1]} color={"orange"} />
+          <Cube position={[0, 2, 0]} size={[1, 1, 1]} color={"red"} />
+          <Cube position={[2, 0, 0]} size={[1, 1, 1]} color={"yellow"} />
+          <Cube position={[0, -2, 0]} size={[1, 1, 1]} color={"brown"} /> */}
+        </group>
+        <Sphere position={[-2, 0, 0]} size={[1, 30, 30]} color={"red"} />
       </Canvas>
     </>
   );
