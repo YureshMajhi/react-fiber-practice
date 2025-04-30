0bb96ae3e758585cc7c02ac311d2bb621b469240
Yuresh <yureshmee@gmail.com>
2025-04-30 13:14:31 +0545

    helper and drei

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useRef, useState } from "react";
 import "./App.css";
 import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  MeshDistortMaterial,
+  MeshWobbleMaterial,
+  OrbitControls,
+  useHelper,
+} from "@react-three/drei";
+import { DirectionalLightHelper } from "three";
 
 const Cube = ({ position, size, color }) => {
   const ref = useRef();
@@ -44,21 +51,43 @@ const Sphere = ({ position, size, color }) => {
   );
 };
 
+const TorusKnot = () => {
+  return (
+    <mesh>
+      <torusKnotGeometry args={[0.8, 0.1, 16]} />
+      <MeshWobbleMaterial factor={1} speed={10} />
+      <OrbitControls />
+    </mesh>
+  );
+};
+
+const Scene = () => {
+  const directionalLightRef = useRef();
+  useHelper(directionalLightRef, DirectionalLightHelper, 0.5, "white");
+  return (
+    <>
+      <directionalLight position={[0, 0, 2]} intensity={0.5} ref={directionalLightRef} />
+      <ambientLight intensity={0.4} />
+
+      <TorusKnot />
+
+      <group position={[3, 0, 0]}>
+        {/* <Cube position={[0, 0, 0]} size={[1, 1, 1]} color={"pink"} /> */}
+        {/* <Cube position={[-2, 0, 0]} size={[1, 1, 1]} color={"orange"} />
+          <Cube position={[0, 2, 0]} size={[1, 1, 1]} color={"red"} />
+          <Cube position={[2, 0, 0]} size={[1, 1, 1]} color={"yellow"} />
+          <Cube position={[0, -2, 0]} size={[1, 1, 1]} color={"brown"} /> */}
+      </group>
+      {/* <Sphere position={[-2, 0, 0]} size={[1, 30, 30]} color={"red"} /> */}
+    </>
+  );
+};
+
 function App() {
   return (
     <>
       <Canvas>
-        <directionalLight position={[0, 0, 2]} intensity={0.5} />
-        <ambientLight intensity={0.4} />
-
-        <group position={[3, 0, 0]}>
-          <Cube position={[0, 0, 0]} size={[1, 1, 1]} color={"pink"} />
-          {/* <Cube position={[-2, 0, 0]} size={[1, 1, 1]} color={"orange"} />
-          <Cube position={[0, 2, 0]} size={[1, 1, 1]} color={"red"} />
-          <Cube position={[2, 0, 0]} size={[1, 1, 1]} color={"yellow"} />
-          <Cube position={[0, -2, 0]} size={[1, 1, 1]} color={"brown"} /> */}
-        </group>
-        <Sphere position={[-2, 0, 0]} size={[1, 30, 30]} color={"red"} />
+        <Scene />
       </Canvas>
     </>
   );
